555898dda19686409f1ba0d6a48c50903af99d4a
玉城 一磨 <k.tamashiro@datumstudio.jp>
2017-05-01 10:06:04 +0900

    [add] できたー

--- pml/sentiment/views.py
+++ pml/sentiment/views.py
@@ -2,6 +2,28 @@ from django.shortcuts import render
 from django.views.generic.edit import FormView
 from .forms import ReviewForm
 from django.utils import timezone
+from .vectorizer import vect
+import numpy as np
+import os
+import pickle
+
+cur_dir = os.path.dirname(__file__)
+clf = pickle.load(open(os.path.join(cur_dir,
+                 'pkl_objects',
+                 'classifier.pkl'), 'rb'))
+label = {0: 'negative', 1: 'positive'}
+
+
+def classify(document):
+    X = vect.transform([document])
+    y = clf.predict(X)[0]
+    proba = np.max(clf.predict_proba(X))
+    return label[y], proba
+
+
+def train(document, y):
+    X = vect.transform([document])
+    clf.partial_fit(X, [y])
 
 
 class IndexView(FormView):
@@ -11,12 +33,13 @@ class IndexView(FormView):
 
     def form_valid(self, form):
         # predictionの実行
+        y, proba = classify(self.request.POST['review_text'])
         return render(self.request,
                     'sentiment/confirm.html',
                     {
                         'response': self.request.POST,
-                        'prediction': '',
-                        'probabillity': ''
+                        'prediction': y,
+                        'probability': proba
                     })
 
 
@@ -25,10 +48,16 @@ def registry(request):
         form = ReviewForm()
         return render(request, 'sentiment/index.html', {'form': form})
 
+    feedback = request.POST['feedback_button']
+    prediction = request.POST['prediction']
     form = ReviewForm(request.POST)
     if form.is_valid():
         review = form.save(commit=False)
-        review.sentiment = 0
+        y = {v: k for k, v in label.items()}[prediction]
+        if feedback == 'Incorrect':
+            y = int(not(y))
+        train(review.review_text, y)
+        review.sentiment = y
         review.created = timezone.now()
         review.save()
         return render(request, 'sentiment/completed.html', {'review': review})
